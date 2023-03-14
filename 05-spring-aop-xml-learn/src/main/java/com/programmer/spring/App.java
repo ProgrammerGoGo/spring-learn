5c8d604c879202b56b37bbedb2bccbec0f5fddb6
programmer_cj <programmer_cj@163.com>
2023-03-14 22:29:25 +0800

    xml方式配置spring aop

--- 05-spring-aop-xml-learn/src/main/java/com/programmer/spring/App.java
+++ 05-spring-aop-xml-learn/src/main/java/com/programmer/spring/App.java
@@ -1,5 +1,24 @@
-package com.programmer.spring;/**
+package com.programmer.spring;
+
+import com.programmer.spring.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
  * @Author programmerGoGo
- * @Description 
- */public class App {
+ * @Description
+ */
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("aspects.xml");
+        UserService bean = applicationContext.getBean(UserService.class);
+
+        System.out.println("=========== doMethod1 ============");
+        bean.doMethod1();
+        System.out.println("=========== doMethod2 ============");
+        bean.doMethod2();
+        System.out.println("=========== doMethod3 ============");
+        bean.doMethod3();
+    }
 }
--- 05-spring-aop-xml-learn/src/main/java/com/programmer/spring/service/UserService.java
+++ 05-spring-aop-xml-learn/src/main/java/com/programmer/spring/service/UserService.java
@@ -1,5 +1,22 @@
-package com.programmer.spring.service;/**
+package com.programmer.spring.service;
+
+/**
  * @Author programmerGoGo
- * @Description 
- */public class UserService {
+ * @Description
+ */
+public class UserService {
+
+    public void doMethod1() {
+        System.out.println("UserService.doMethod1()");
+    }
+
+    public String doMethod2() {
+        System.out.println("UserService.doMethod2()");
+        return "hello world";
+    }
+
+    public String doMethod3() throws Exception {
+        System.out.println("UserService.doMethod3()");
+        throw new Exception("some exception");
+    }
 }
