cc04f44f56449940ed88cb7356b4d3e2e50f351f
programmer_cj <programmer_cj@163.com>
2023-04-06 23:31:54 +0800

    配置类

--- 04-spring-aop-annotation-learn/src/main/java/com/programmer/spring/App.java
+++ 04-spring-aop-annotation-learn/src/main/java/com/programmer/spring/App.java
@@ -1,5 +1,6 @@
 package com.programmer.spring;
 
+import com.programmer.spring.config.AppConfig;
 import com.programmer.spring.service.CgLibProxyService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,11 +10,12 @@ import org.springframework.context.annotation.ComponentScan;
  * @Author programmerGoGo
  * @Description
  */
-@ComponentScan("com.programmer.spring")
+//@ComponentScan("com.programmer.spring")
 public class App {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(App.class);
+//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         CgLibProxyService bean = applicationContext.getBean(CgLibProxyService.class);
         System.out.println("======doMethod1======");
         bean.doMethod1("hello");
---
+++ 04-spring-aop-annotation-learn/src/main/java/com/programmer/spring/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.programmer.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author programmerGoGo
+ * @Description
+ */
+@ComponentScan("com.programmer.spring")
+@Configuration
+public class AppConfig {
+}
